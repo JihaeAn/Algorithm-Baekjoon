b5a4b13407c7015f0ea449c21d37f3f021b8e609
jihae <dkswlgo6615@naver.com>
2025-06-26 19:41:29 +0900

    백준 14215

---
+++ baekjoon/level_10/A_14215_1.java
@@ -0,0 +1,32 @@
+package baekjoon.level_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 처음에 풀었던 방식이다. 쓸데없는 코드가 보인다.
+ */
+public class A_14215_1 {
+    public static void main(String[] args) throws IOException {
+        int[] arr = new int[3];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        arr[0] = Integer.parseInt(s.nextToken());
+        arr[1] = Integer.parseInt(s.nextToken());
+        arr[2] = Integer.parseInt(s.nextToken());
+
+        Arrays.sort(arr);
+
+        if (arr[0] == arr[1] && arr[1] == arr[2]) {
+            System.out.println(arr[0] + arr[1] + arr[2]);
+        } else if (arr[0] + arr[1] <= arr[2]) {
+            System.out.println((arr[0] + arr[1]) * 2  - 1);
+        } else {
+            System.out.println(arr[0] + arr[1] + arr[2]);
+        }
+    }
+}
